a5116c3c4524cae01149cb0c17e7eb2b5b6e58f6
RahmatPrakoso276 <prakosorahmat142@gmail.com>
2024-10-22 00:54:21 +0700

    updated src image

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Isu Lingkungan</title>
-    <link rel="icon" href="{{ asset('icon.svg') }}">
+    <link rel="icon" href="{{ asset('images/icon.svg') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
